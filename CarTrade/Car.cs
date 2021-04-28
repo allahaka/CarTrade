6fe943bc6ad37f998273dbc339f353ee782954f0
allahaka <allahaka90@gmail.com>
2021-04-28 16:09:44 +0200

    name changes, init of class

--- CarTrade/Car.cs
+++ CarTrade/Car.cs
@@ -14,27 +14,39 @@ namespace CarTrade
         public readonly string colour;
         public readonly string segment;
         public readonly string type;
+        public readonly int cargoSpace;
         public string owner;
 
         //parts for car
         public Part[] parts;
 
+        public Car(decimal value, string brand, decimal mileage, string colour, string segment, string type, int cargoSpace=0) {
+            this.value = value;
+            this.brand = brand;
+            this.mileage = mileage;
+            this.colour = colour;
+            this.segment = segment;
+            this.type = type;
+            this.cargoSpace = cargoSpace;
+            this.CreateParts();
+        }
+
         //helper method
-        private bool needRepairing()
+        private bool NeedRepairing()
         {
             Random rng = new Random();
-            return rng.Next(100) <= 50 ? true : false;
+            return rng.Next(100) <= 50;
         }
 
-        private void createParts() {
-            this.parts[0] = new Part("Break", needRepairing(), 10);
-            this.parts[1] = new Part("Suspension", needRepairing(), 20);
-            this.parts[2] = new Part("Engine", needRepairing(), 100);
-            this.parts[3] = new Part("Body", needRepairing(), 50);
-            this.parts[4] = new Part("Gearbox", needRepairing(), 50);
+        public void CreateParts() {
+            this.parts[0] = new Part("Break", NeedRepairing(), 10);
+            this.parts[1] = new Part("Suspension", NeedRepairing(), 20);
+            this.parts[2] = new Part("Engine", NeedRepairing(), 100);
+            this.parts[3] = new Part("Body", NeedRepairing(), 50);
+            this.parts[4] = new Part("Gearbox", NeedRepairing(), 50);
         }
 
-        private decimal finalPrice() {
+        private decimal FinalPrice() {
             decimal final = this.value;
             for (int i = 0; i < 5; i++) {
                 final = this.parts[i].needRepairing ? final + this.value * this.parts[i].valueIncrease : final;
@@ -42,10 +54,10 @@ namespace CarTrade
             return final;
         }
 
-        private string[] displayCar() {
+        public string[] DisplayCar() {
             string firstLine = $"The {this.colour} {this.brand} \n {this.type} car out of {this.segment} segment.";
-            string secondLine = $"Price: {this.finalPrice()}";
-            string parts = "";
+            string secondLine = $"Price: {this.FinalPrice()}";
+            string parts = "Status of parts: \n";
             for (int i = 0; i < 5; i++) {
                 parts += $"{this.parts[i]}: {(this.parts[i].needRepairing ? "Repair is needed" : "Perfect")} \n";
             }
