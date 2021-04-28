54247079ecab35bd6400a72466e8eab82076631b
allahaka <allahaka90@gmail.com>
2021-04-28 16:09:50 +0200

    Create Client.cs

---
+++ CarTrade/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarTrade
+{
+    class Client
+    {
+        public readonly string name;
+        public readonly decimal cash;
+        public readonly String interestedInType;
+        public readonly String[] interestedIn;
+        public readonly Part[] acceptedDamagedParts;
+
+        public Client(string name, decimal cash, String interestedInType, String[] interestedIn, Part[] acceptedDamagedParts) {
+            this.name = name;
+            this.cash = cash;
+            this.interestedInType = interestedInType;
+            this.interestedIn = interestedIn;
+            this.acceptedDamagedParts = acceptedDamagedParts;
+        }
+    }
+}
