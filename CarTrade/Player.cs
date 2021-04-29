84832e4134d25cd1ec5ea8f9a7a1703e4687d0bc
allahaka <allahaka90@gmail.com>
2021-04-29 18:01:48 +0200

    Create Player.cs

---
+++ CarTrade/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarTrade
+{
+    class Player
+    {
+        public decimal account;
+        public Car[] ownedCars;
+    }
+}
