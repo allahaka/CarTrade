057b5f00a76901af1d18922798d6a6e70d5e5091
allahaka <allahaka90@gmail.com>
2021-05-03 19:55:24 +0200

    Update Player.cs

--- CarTrade/Player.cs
+++ CarTrade/Player.cs
@@ -6,7 +6,13 @@ namespace CarTrade
 {
     class Player
     {
+        public string name;
         public decimal account;
         public Car[] ownedCars;
+
+        public Player(string name, decimal account){
+            this.name = name;
+            this.account = account;
+        }
     }
 }
