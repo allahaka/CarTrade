febd28910f95f11c52b7fd3dfc74e41f23ef5622
allahaka <allahaka90@gmail.com>
2021-05-03 19:54:01 +0200

    Finish CarGenerator and simplify switches

--- CarTrade/CarGenerator.cs
+++ CarTrade/CarGenerator.cs
@@ -1,31 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Reflection;
 using System.Text;
 
 namespace CarTrade
 {
     class CarGenerator
     {
-        public Car GenerateCar() {
+        readonly Helpers help = new Helpers();
 
-            string type = CarType();
-            string segment = CarSegment();
-            string brand = GenerateBrand(segment);
-            decimal value = GenerateBasePrice(segment, type);
-            decimal mileage = RandomDecimal(new Random(), 6, 2);
-            string colour = GetRandomColorName();
-            int cargoSpace = type == "cargo" ? RandomNumber(10000, 1000) : 0;
-            Car car = new Car(value, brand, mileage, colour, segment, type, cargoSpace);
-            car.CreateParts();
-            return car;
+        public List<Car> GenerateCar(int amount) {
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < amount; i++) {
+                string type = CarType();
+                string segment = CarSegment();
+                string brand = GenerateBrand(segment);
+                decimal value = GenerateBasePrice(segment, type);
+                decimal mileage = help.RandomDecimal(new Random(), 6, 2);
+                string colour = help.GetRandomColorName();
+                int cargoSpace = type == "cargo" ? help.RandomNumber(10000, 1000) : 0;
+                Car car = new Car(value, brand, mileage, colour, segment, type, cargoSpace);
+                car.CreateParts();
+                cars.Add(car); 
+            }
+
+            return cars;
         }
 
         public string CarType() {
 
-            int typeRand = RandomNumber(1000);
+            int typeRand = help.RandomNumber(1000);
 
             if (typeRand <= 600) {
                 return "normal";
@@ -38,7 +42,7 @@ namespace CarTrade
 
         public string CarSegment() {
 
-            int segRand = RandomNumber(1000);
+            int segRand = help.RandomNumber(1000);
 
             if (segRand <= 500) {
                 return "budget";
@@ -55,102 +59,44 @@ namespace CarTrade
             string[] standard = new string[9] { "Audi", "BMW", "Jeep", "Land Rover", "Lexus", "Mercedes-Benz", "Volkswagen", "Volvo", "Alfa Romeo" };
             string[] premium = new string[8] { "Aston Martin", "Bugatti", "Dodge", "Ferrari", "Lamborghini", "McLaren", "Rolls-Royce", "Tesla" };
 
-            switch(segment){
-                case "budget":
-                    return budget[RandomNumber(budget.Length)];
-                case "standard":
-                    return standard[RandomNumber(standard.Length)];
-                case "premium":
-                    return premium[RandomNumber(premium.Length)];
-                default:
-                    return budget[RandomNumber(budget.Length)];
-            }
+            return segment switch {
+                "budget" => budget[help.RandomNumber(budget.Length)],
+                "standard" => standard[help.RandomNumber(standard.Length)],
+                "premium" => premium[help.RandomNumber(premium.Length)],
+                _ => budget[help.RandomNumber(budget.Length)],
+            };
         }
 
         public decimal GenerateBasePrice(string segment, string type) {
-            switch(segment){
-                case "budget":
-                    return GenerateBudgetPrice(type);
-                case "standard":
-                    return GenerateStandardPrice(type);
-                case "premium":
-                    return GeneratePremiumPrice(type);
-                default:
-                    return GenerateBudgetPrice(type);
-            }
+            return segment switch {
+                "budget" => GenerateBudgetPrice(type),
+                "standard" => GenerateStandardPrice(type),
+                "premium" => GeneratePremiumPrice(type),
+                _ => GenerateBudgetPrice(type),
+            };
         }
 
         private decimal GenerateBudgetPrice(string type){
-            decimal value = Convert.ToDecimal(RandomNumber(30000, 15000));
+            decimal value = Convert.ToDecimal(help.RandomNumber(30000, 15000));
             return DiffPriceOnType(value, type);
         }
 
         private decimal GenerateStandardPrice(string type){
-            decimal value = Convert.ToDecimal(RandomNumber(100000, 30000));
+            decimal value = Convert.ToDecimal(help.RandomNumber(100000, 30000));
             return DiffPriceOnType(value, type);
         }
 
         private decimal GeneratePremiumPrice(string type) {
-            decimal value = Convert.ToDecimal(RandomNumber(625000, 70000));
+            decimal value = Convert.ToDecimal(help.RandomNumber(625000, 70000));
             return DiffPriceOnType(value, type);
         }
 
         private decimal DiffPriceOnType(decimal value, string type){
-            switch (type) {
-                case "cargo":
-                    return Decimal.Multiply(value, 1.1m);
-                case "motorcycle":
-                    return Decimal.Multiply(value, 0.9m);
-                default:
-                    return value;
-            }
-        }
-
-        //helper function
-        public int RandomNumber(int max, int min = 0) {
-            Random rng = new Random();
-            return rng.Next(min, max);
-        }
-
-        static decimal RandomDecimal(Random randomNumberGenerator, int precision, int scale) {
-            if (randomNumberGenerator == null)
-                throw new ArgumentNullException("randomNumberGenerator");
-            if (!(precision >= 1 && precision <= 28))
-                throw new ArgumentOutOfRangeException("precision", precision, "Precision must be between 1 and 28.");
-            if (!(scale >= 0 && scale <= precision))
-                throw new ArgumentOutOfRangeException("scale", precision, "Scale must be between 0 and precision.");
-
-            Decimal d = 0m;
-            for (int i = 0; i < precision; i++) {
-                int r = randomNumberGenerator.Next(0, 10);
-                d = d * 10m + r;
-            }
-            for (int s = 0; s < scale; s++) {
-                d /= 10m;
-            }
-            return d;
-        }
-
-        //Colour generation
-        static readonly Color[] Colors =
-            typeof(Color).GetProperties(BindingFlags.Public | BindingFlags.Static)
-           .Select(propInfo => propInfo.GetValue(null, null))
-           .Cast<Color>()
-           .ToArray();
-
-        static readonly string[] ColorNames =
-            typeof(Color).GetProperties(BindingFlags.Public | BindingFlags.Static)
-            .Select(propInfo => propInfo.Name)
-            .ToArray();
-
-        private Random rand = new Random();
-
-        public Color GetRandomColor() {
-            return Colors[rand.Next(0, Colors.Length)];
-        }
-
-        public string GetRandomColorName() {
-            return ColorNames[rand.Next(0, Colors.Length)];
+            return type switch {
+                "cargo" => Decimal.Multiply(value, 1.1m),
+                "motorcycle" => Decimal.Multiply(value, 0.9m),
+                _ => value,
+            };
         }
     }
 }
