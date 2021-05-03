4be3ab7a692f5a340bd2d2e2654ae0fab5622426
allahaka <allahaka90@gmail.com>
2021-05-03 20:08:00 +0200

    added moves counter for player

--- CarTrade/Player.cs
+++ CarTrade/Player.cs
@@ -9,10 +9,16 @@ namespace CarTrade
         public string name;
         public decimal account;
         public Car[] ownedCars;
+        public int amountOfMoves;
 
         public Player(string name, decimal account){
             this.name = name;
             this.account = account;
+            this.amountOfMoves = 0;
+        }
+
+        public void MadeMove(){
+            this.amountOfMoves += 1;
         }
     }
 }
