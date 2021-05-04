ca199084458bb21112be14626534e62f74b508b6
allahaka <allahaka90@gmail.com>
2021-05-04 10:22:58 +0200

    remove unused usings variable name changes

--- CarTrade/Menus.cs
+++ CarTrade/Menus.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace CarTrade
 {
     class Menus
     {
         public List<Player> players;
+        public Player currentPlayer;
 
         public void AddPlayers(List<Player> players){
             this.players = players;
@@ -18,19 +18,19 @@ namespace CarTrade
 
         public List<string> StartGame(){
             List<string> result = new List<string>();
-            int players;
+            int numberPlayers;
 
             Console.WriteLine("How many players: ");
             
-            while (!int.TryParse(Console.ReadLine(), out players)) {
+            while (!int.TryParse(Console.ReadLine(), out numberPlayers)) {
                 Console.WriteLine("Please Enter a valid numerical value:");
             }
 
-            result.Add(players.ToString());
+            result.Add(numberPlayers.ToString());
 
             Clean();
 
-            for(int i=0; i<players; i++){
+            for(int i=0; i<numberPlayers; i++){
                 Console.WriteLine($"Enter the names of player{i}: ");
                 string name = Console.ReadLine();
                 result.Add(name);
@@ -61,83 +61,91 @@ namespace CarTrade
         }
 
         public void MainMenu() {
-            Console.WriteLine("1. [B]uy a car                           2. [V]iew Owned cars");
-            Console.WriteLine("3. [R]epair a car                        4. Buy an [A]d");
-            Console.WriteLine("5. [S]ell a car                          6. [C]heck account");
-            Console.WriteLine("7. Check [H]istory of actions            8. Check sum of all [P]ayments for owned cars");
+            Console.WriteLine($"Player: {currentPlayer.name}, Moves: {currentPlayer.amountOfMoves}, Account: {currentPlayer.account}");
+            Console.WriteLine("[B]uy a car                           [V]iew Owned cars");
+            Console.WriteLine("[R]epair a car                        Buy an [A]d");
+            Console.WriteLine("[S]ell a car                          [C]heck account");
+            Console.WriteLine("Check [H]istory of actions            Check sum of all [P]ayments for owned cars");
+            Console.WriteLine("[Q]uit");
             ConsoleKeyInfo ck = Console.ReadKey();
 
             switch (ck.Key) {
                 case ConsoleKey.B:
                     Clean();
-                    this.BuyCarMenu();
+                    BuyCarMenu();
                     break;
                 case ConsoleKey.V:
                     Clean();
-                    this.ViewOwnedCarsMenu();
+                    ViewOwnedCarsMenu();
                     break;
                 case ConsoleKey.R:
                     Clean();
-                    this.RepairCarMenu();
+                    RepairCarMenu();
                     break;
                 case ConsoleKey.A:
                     Clean();
-                    this.BuyAdMenu();
+                    BuyAdMenu();
                     break;
                 case ConsoleKey.S:
                     Clean();
-                    this.SellCarMenu();
+                    SellCarMenu();
                     break;
                 case ConsoleKey.C:
                     Clean();
-                    this.CheckAccountMenu();
+                    CheckAccountMenu();
                     break;
                 case ConsoleKey.H:
                     Clean();
-                    this.CheckHistoryMenu();
+                    CheckHistoryMenu();
                     break;
                 case ConsoleKey.P:
                     Clean();
-                    this.CheckPaymentsMenu();
+                    CheckPaymentsMenu();
+                    break;
+                case ConsoleKey.Q:
+                    Environment.Exit(0);
                     break;
             } 
         }
 
-        public void BuyCarMenu() { 
-        
+        public void BuyCarMenu(){
+            Console.WriteLine("Buy Car Menu");
+            ConsoleKeyInfo ck = Console.ReadKey();
         }
 
-        public void ViewOwnedCarsMenu()
-        {
-
+        public void ViewOwnedCarsMenu(){
+            Console.WriteLine("Owned Cars Menu");
+            ConsoleKeyInfo ck = Console.ReadKey();
         }
 
-        public void RepairCarMenu()
-        {
-
+        public void RepairCarMenu(){
+            Console.WriteLine("Repair Car Menu");
+            ConsoleKeyInfo ck = Console.ReadKey();
         }
 
-        public void BuyAdMenu()
-        {
-
+        public void BuyAdMenu(){
+            Console.WriteLine("Buy Ad Menu");
+            ConsoleKeyInfo ck = Console.ReadKey();
         }
 
-        public void SellCarMenu()
-        {
-
+        public void SellCarMenu(){
+            Console.WriteLine("Sell Car Menu");
+            ConsoleKeyInfo ck = Console.ReadKey();
         }
 
-        public void CheckAccountMenu()
-        {
-
+        public void CheckAccountMenu(){
+            Console.WriteLine("Check Account Menu");
+            ConsoleKeyInfo ck = Console.ReadKey();
         }
-        public void CheckHistoryMenu()
-        {
 
+        public void CheckHistoryMenu(){
+            Console.WriteLine("Check History Menu");
+            ConsoleKeyInfo ck = Console.ReadKey();
         }
-        public void CheckPaymentsMenu()
-        {
 
+        public void CheckPaymentsMenu(){
+            Console.WriteLine("Check Payments Menu");
+            ConsoleKeyInfo ck = Console.ReadKey();
         }
     }
 }
