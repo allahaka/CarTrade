11d2246fcd21452a92b8894f0ce7a3690a43eaa0
allahaka <allahaka90@gmail.com>
2021-05-06 18:51:16 +0200

    List of cars not array

--- CarTrade/Player.cs
+++ CarTrade/Player.cs
@@ -1,15 +1,18 @@
-﻿namespace CarTrade
+﻿using System.Collections.Generic;
+
+namespace CarTrade
 {
     class Player
     {
         public string name;
         public decimal account;
-        public Car[] ownedCars;
+        public List<Car> ownedCars;
         public int amountOfMoves;
 
         public Player(string name, decimal account){
             this.name = name;
             this.account = account;
+            this.ownedCars = new List<Car>();
             this.amountOfMoves = 0;
         }
 
